f094773f9fcdcf20281e8cf3ebbadbe0418c115e
Joel-Crunk <joel.crunk@outlook.com>
2019-03-27 14:55:30 -0400

    Add project files.

---
+++ OleManCrunksFarm/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OleManCrunksFarm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
